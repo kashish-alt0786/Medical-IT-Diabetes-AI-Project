d3530f3b2847d048470b46618a6ae46c3b4c4495
kashish-alt0786 <kashishkhann38@gmail.com>
2026-07-20 12:04:51 +0530

    Update model training to use XGBoost and joblib
    Refactor model training to use XGBoost and save model.

--- train_model.py
+++ train_model.py
@@ -1,7 +1,13 @@
+import pandas as pd
+import xgboost as xgb
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 df = pd.read_csv(...)
-
 X = ...
-
 y = ...
-
-model.fit(X,y)
+model = xgb.XGBClassifier(...)
+model.fit(...)
+joblib.dump(model, "model.pkl")
